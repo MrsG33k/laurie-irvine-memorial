be713acac60fa5717bef1d5aaf29aa295f0f4441
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-06-01 17:20:39 +0100

    Add: Registered Admin and created superuser account

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MemorialPost
 
-# Register your models here.
+# Register the model so it can be managed in the Admin Dashboard
+
+
+@admin.register(MemorialPost)
+class MemorialPostAdmin(admin.ModelAdmin):
+    # Determines what details admin see when looking at list of posts
+    list_display = ('author_name', 'relationship', 'light_candle', 'date_created')
+
+    # Adds a filter for easier viewing
+    list_filter = ('light_candle', 'relationship', 'date_created')
+
+    # Allows searching on certain fields
+    search_fields = ('author_name', 'tribute_text')
