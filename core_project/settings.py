0322653d20a87a8217d6eb85a179d6aeb38d9d1d
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-06-15 17:02:24 +0100

    Fix: Adjusted STATIC_ROOT path in settings.py

--- core_project/settings.py
+++ core_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Telling Django where to gather files for production
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Telling Django about the app folder structure
 STATICFILES_DIRS = [BASE_DIR / 'tracker' / 'static']
