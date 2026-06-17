596a2291d0228987b542ff8e22bf4505e2af2687
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-06-17 18:57:50 +0100

    Fix: Spacing error on import lines on settings.py

--- core_project/settings.py
+++ core_project/settings.py
@@ -1,6 +1,8 @@
-
 import os
 from pathlib import Path
+
+# Calculates the absolute filepath to project main folder
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
